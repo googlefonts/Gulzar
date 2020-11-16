02823943b7345f7e3b75c87d1d3aaad976fc1577
Simon Cozens <simon@simon-cozens.org>
2020-11-16 14:35:42 +0000

    Updates to arabic linter

--- lint-arabic.py
+++ lint-arabic.py
@@ -1,12 +1,18 @@
-import glyphsLib
+# MenuTitle: Lint Arabic font
+# -*- coding: utf-8 -*-
+__doc__ = """
+Check for errors in a Qalmi-style Arabic font
+"""
+
 import sys
-import copy
 import re
-import csv
 
-
-infile = sys.argv[1]
-gsfont = glyphsLib.GSFont(infile)
+if "Glyphs" not in globals():
+  import glyphsLib
+  infile = sys.argv[1]
+  gsfont = glyphsLib.GSFont(infile)
+else:
+  gsfont = Glyphs.font
 
 def has_anchor(n, anchors):
   for a in anchors:
@@ -16,39 +22,8 @@ def has_anchor(n, anchors):
 reachable = set([])
 rules = {}
 
-with open("rules.csv") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for line in reader:
-        left_glyph = line["Left Glyph"]
-        if not left_glyph in gsfont.glyphs:
-            continue
-        remainder = list(line.items())[1:]
-        for (g, v) in remainder:
-            old = g + "1"
-            if v == "1" or v == 1 or not v:
-                continue
-            replacement = g + str(v)
-            if not replacement in gsfont.glyphs:
-                continue
-            if not old in rules:
-                rules[old] = {}
-            if not replacement in rules[old]:
-                rules[old][replacement] = []
-            rules[old][replacement].append(left_glyph)
-
-for oldglyph in rules:
-    for replacement in rules[oldglyph]:
-        context = rules[oldglyph][replacement]
-        reachable |= set(context)
-        reachable |= set([oldglyph, replacement])
-
-
 for g in gsfont.glyphs:
   l = g.layers[0]
-  if not l.paths:
-    continue
-  if not g.name in reachable:
-    continue
   if re.search(r"^.*f\d+$", g.name):
     if not has_anchor("entry", l.anchors):
       print("Final glyph %s has no entry anchor" % g.name)
@@ -60,3 +35,26 @@ for g in gsfont.glyphs:
       print("Medial glyph %s has no exit anchor" % g.name)
     if not has_anchor("entry", l.anchors):
       print("Medial glyph %s has no entry anchor" % g.name)
+
+  if not g.category == "Mark":
+    continue
+  for a in l.anchors:
+    if a.position.x == 0 and a.position.y == 0:
+      print("Badly positioned anchor %s on glyph %s" % (a, g.name))
+  if g.name not in ["sda", "dda", "tda", "sdb", "ddb", "tdb", "sdb.yb", "ddb.yb", "tdb.yb"]:
+    if has_anchor("top", l.anchors) and g.name != "SHADDA":
+      print("%s should not have a top anchor" % g.name)
+    if has_anchor("bottom", l.anchors):
+      print("%s should not have a bottom anchor" % g.name)
+
+  if not has_anchor("top", l.anchors) and g.name == "SHADDA":
+    print("%s should have a top anchor" % g.name)
+
+  # Ensure above marks have _top anchors
+  if g.name in ["SHADDA", "DAMMA"]:
+    if not has_anchor("_top", l.anchors):
+      print("%s should have a _top anchor" % g.name)
+
+  if g.name in []:
+    if not has_anchor("_bottom", l.anchors):
+      print("%s should have a _bottom anchor" % g.name)
